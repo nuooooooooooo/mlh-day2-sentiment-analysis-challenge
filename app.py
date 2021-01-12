e873d0aaf205e89e544660bf2d549638b8a1e502
Manon <31137629+web-Nuo@users.noreply.github.com>
2021-01-12 21:41:33 +0100

    Fixed bugs

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from textblob import TextBlob
 import tweepy
 
-
-# Access Twitter.api through Tweepy
-# user has to insert their API key and other tokens
 api_key = ''
 api_secret = ''
 acces_token = ''
@@ -13,7 +10,7 @@ auth.set_access_token(acces_token, acces_token_secret)
 api = tweepy.API(auth)
 
 # initialize variables
-tweet_count = 50
+tweet_count = 30
 mood, opinion = 0.0, 0.0
 mood_tweet_nbr = 0
 user = api.me()
@@ -28,24 +25,24 @@ for tweet in timeline_tweets:
         mood += analysis.sentiment.polarity
         opinion += analysis.sentiment.subjectivity
         mood_tweet_nbr += 1
-
+        print(tweet.text)
 # calculate the average sentiment and subjectivity of tweets on my time line
-    mood_avg = round(mood / mood_tweet_nbr, 1)
-    opinion_avg = round(opinion / mood_tweet_nbr, 1)
+mood_avg = mood / mood_tweet_nbr
+opinion_avg = opinion / mood_tweet_nbr
 
 
 # evaluate timeline sentiment and subjectivity average on a scale of 1 to 5, 6 is for any numbers not in sentiment analysis range
 def timeline_avg_scale(avg):
-    
+
     index = 0
 
     # only evaluates floats
     if not isinstance(avg, float):
         index = 5
         return index
-        
+
     if(avg < -0.6):
-        if (avg< -1.0):
+        if (avg < -1.0):
             index = 5
         else:
             index = 0
@@ -68,8 +65,10 @@ mood_index = timeline_avg_scale(mood_avg)
 opinion_index = timeline_avg_scale(opinion_avg)
 
 # lists of words to desribe sentiment and subjectivity
-mood_list = ["extremely negative", "negative", "moderate", "positive", "extremely positive", "unknown"]
-opinion_list = ["totally objective", "objective", "neutral", "subjective", "highly subjective", "unknown"]
+mood_list = ["extremely negative", "negative", "moderate",
+             "positive", "extremely positive", "unknown"]
+opinion_list = ["totally objective", "objective",
+                "neutral", "subjective", "highly subjective", "unknown"]
 
 
 # data to be output on the user's screen
@@ -78,4 +77,5 @@ output_opinion = opinion_list[opinion_index]
 output_str = 'Howdy, {}! Right now, tweets on your timeline are {} and {}.'
 
 # print result on screen
-print(output_str.format(user.screen_name,output_mood,output_opinion))
+print('average sentiment {} and average subjectivity {}'.format(mood_avg, opinion_avg))
+print(output_str.format(user.screen_name, output_mood, output_opinion))
